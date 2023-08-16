f25dd1efa44f894abd804fe1ded036fdb4f7bcf3
Pedro Figueiredo <pedro.figueiredo35@gmail.com>
2023-08-16 15:33:51 +0100

    added score system plus modifications to the game over screen

--- JS/main.js
+++ JS/main.js
@@ -24,31 +24,39 @@ class Player {
     parentElm.appendChild(this.playerSpaceShip);
   }
   moveLeft() {
-    this.positionX--;
-    this.playerSpaceShip.style.left = this.positionX + "vw";
+    if (this.positionX > 0) {
+      this.positionX--;
+      this.playerSpaceShip.style.left = this.positionX + "vw";
+    }
   }
   moveRight() {
-    this.positionX++;
-    this.playerSpaceShip.style.left = this.positionX + "vw";
+    if (this.positionX < 94) {
+      this.positionX++;
+      this.playerSpaceShip.style.left = this.positionX + "vw";
+    }
   }
   moveUp() {
-    this.positionY++;
-    this.playerSpaceShip.style.bottom = this.positionY + "vh";
+    if (this.positionY < 95) {
+      this.positionY++;
+      this.playerSpaceShip.style.bottom = this.positionY + "vh";
+    }
   }
   moveDown() {
-    this.positionY--;
-    this.playerSpaceShip.style.bottom = this.positionY + "vh";
+    if (this.positionY > 0) {
+      this.positionY--;
+      this.playerSpaceShip.style.bottom = this.positionY + "vh";
+    }
   }
   shoot() {
     const projectile = new PlayerProjectiles();
-    projectileArr.push(projectile)
+    projectileArr.push(projectile);
   }
 }
 
 class PlayerProjectiles {
   constructor() {
-    this.width = 2;
-    this.height = 1;
+    this.width = 3;
+    this.height = 4.4;
     this.positionX = player.positionX;
     this.positionY = player.positionY;
     this.projectileElement = null;
@@ -58,18 +66,18 @@ class PlayerProjectiles {
   createDomElement() {
     this.projectileElement = document.createElement("div");
 
-    this.projectileElement.className = "player-projectiles"
+    this.projectileElement.className = "player-projectiles";
     this.projectileElement.style.width = this.width + "vw";
     this.projectileElement.style.height = this.height + "vh";
     this.projectileElement.style.left = this.positionX + "vw";
     this.projectileElement.style.bottom = this.positionY + "vh";
-    this.projectileElement.style.background = "red";
+    
 
     const parentElm = document.getElementById("board");
     parentElm.appendChild(this.projectileElement);
   }
   moveUp() {
-    this.positionY += 0.1; 
+    this.positionY += 0.1;
     this.projectileElement.style.bottom = this.positionY + "vh";
   }
 }
@@ -103,30 +111,62 @@ class Enemy {
   }
 }
 
+class Score {
+  constructor() {
+    this.points = 0;
+    this.positionX = 80;
+    this.positionY = 90;
+    this.width = 10;
+    this.height = 5;
+    this.scoreElement = null;
+
+    this.createDomElement();
+  }
+  createDomElement() {
+    this.scoreElement = document.createElement("div");
+
+    this.scoreElement.className = "player-score";
+    this.scoreElement.style.width = this.width + "vw";
+    this.scoreElement.style.height = this.height + "vh";
+    this.scoreElement.style.left = this.positionX + "vw";
+    this.scoreElement.style.bottom = this.positionY + "vh";
+    this.scoreElement.textContent = "Score: " + this.points;
+    const parentElm = document.getElementById("board");
+    parentElm.appendChild(this.scoreElement);
+  }
+  updateScore(value) {
+    this.points += value;
+    this.scoreElement.textContent = "Score: " + this.points;
+  }
+}
+const playerScore = new Score();
 const player = new Player();
 const projectileArr = [];
 const EnemyArr = [];
 setInterval(() => {
-  projectileArr.forEach(function (projectile,ind) {
+  projectileArr.forEach(function (projectile, ind) {
     projectile.moveUp();
     if (projectile.positionY > 100 - projectile.height) {
       projectile.projectileElement.remove();
       projectileArr.shift();
     }
-    EnemyArr.forEach(function(enemy,i){
-      if( projectile.positionX < enemy.positionX + enemy.width &&
-      projectile.positionX + projectile.width > enemy.positionX &&
-      projectile.positionY < enemy.positionY + enemy.height &&
-      projectile.positionY + projectile.height > enemy.positionY){
+    EnemyArr.forEach(function (enemy, i) {
+      if (
+        projectile.positionX < enemy.positionX + enemy.width &&
+        projectile.positionX + projectile.width > enemy.positionX &&
+        projectile.positionY < enemy.positionY + enemy.height &&
+        projectile.positionY + projectile.height > enemy.positionY
+      ) {
         enemy.enemySpaceShip.remove();
-        EnemyArr.splice(i,1)
+        EnemyArr.splice(i, 1);
         projectile.projectileElement.remove();
-        projectileArr.splice(ind,1)
+        projectileArr.splice(ind, 1);
+        playerScore.updateScore(5);
       }
-    })
+    });
   });
 }, 1);
- 
+
 
 setInterval(() => {
   const newEnemy = new Enemy();
@@ -151,6 +191,7 @@ setInterval(() => {
       location.href = "./GameOver.html";
     }
   });
+
 }, 10);
 
 document.addEventListener("keydown", (e) => {
@@ -163,6 +204,6 @@ document.addEventListener("keydown", (e) => {
   } else if (e.key === "ArrowUp") {
     player.moveUp();
   } else if (e.key === " ") {
-    player.shoot()
+    player.shoot();
   }
 });
